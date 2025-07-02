cd44884532032b218c5bb3d421c64b9903f3a001
IDK <114621472+yamada-sexta@users.noreply.github.com>
2025-07-02 14:53:54 -0500

    Cleanup

--- web_ui.py
+++ web_ui.py
@@ -1,23 +1,21 @@
 import fairseq
 import torch
 import shared
-from tabs.ckpt_processing_tab import create_ckpt_processing_tab
-from tabs.faq_tab import create_faq_tab
-from tabs.inference_tab import create_inference_tab
-
 import gradio as gr
-
-from tabs.onnx_tab import create_onnx_tab
+import git
+from tabs.ckpt_processing_tab import create_ckpt_processing_tab
+# from tabs.faq_tab import create_faq_tab
+# from tabs.onnx_tab import create_onnx_tab
+from tabs.inference_tab import create_inference_tab
 from tabs.train_tab import create_train_tab
 from tabs.vocal_tab import create_vocal_tab
 
-from fairseq.data.dictionary import Dictionary
+
 torch.serialization.add_safe_globals([fairseq.data.dictionary.Dictionary])
-import git
-repo = git.Repo(search_parent_directories=True)
 
 
 with gr.Blocks(title="RVC WebUI Fork") as app:
+    repo = git.Repo(search_parent_directories=True)
     gr.Markdown(f"## RVC WebUI Fork ({repo.active_branch}) ({repo.head.object.hexsha[:7]})" )
     with gr.Tabs():
         create_inference_tab(app=app)
