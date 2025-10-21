bae148be15e00f06ea21b380c9c1b4f3e6a3fec3
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-10-20 23:36:34 -0400

    cleanup

--- infer/lib/rmvpe.py
+++ infer/lib/rmvpe.py
@@ -1,9 +1,14 @@
+import logging
 import os
 from io import BytesIO
-from typing import List
+from time import time as ttime
 
 import numpy as np
 import torch
+import torch.nn.functional as F
+from librosa.util import pad_center
+from scipy.signal import get_window
+from torch import nn
 
 from infer.lib import jit
 
@@ -16,12 +21,6 @@ try:
         ipex_init()
 except Exception:  # pylint: disable=broad-exception-caught
     pass
-import logging
-
-import torch.nn.functional as F
-from librosa.util import pad_center
-from scipy.signal import get_window
-from torch import nn
 
 logger = logging.getLogger(__name__)
 
@@ -156,9 +155,6 @@ class STFT(torch.nn.Module):
         return reconstruction
 
 
-from time import time as ttime
-
-
 class BiGRU(nn.Module):
     def __init__(self, input_features, hidden_features, num_layers):
         super(BiGRU, self).__init__()
@@ -226,7 +222,7 @@ class Encoder(nn.Module):
         self.bn = nn.BatchNorm2d(in_channels, momentum=momentum)
         self.layers = nn.ModuleList()
         self.latent_channels = []
-        for i in range(self.n_encoders):
+        for _ in range(self.n_encoders):
             self.layers.append(
                 ResEncoderBlock(
                     in_channels, out_channels, kernel_size, n_blocks, momentum=momentum
@@ -240,9 +236,9 @@ class Encoder(nn.Module):
         self.out_channel = out_channels
 
     def forward(self, x: torch.Tensor):
-        concat_tensors: List[torch.Tensor] = []
+        concat_tensors: list[torch.Tensor] = []
         x = self.bn(x)
-        for i, layer in enumerate(self.layers):
+        for layer in self.layers:
             t, x = layer(x)
             concat_tensors.append(t)
         return x, concat_tensors
@@ -256,14 +252,14 @@ class ResEncoderBlock(nn.Module):
         self.n_blocks = n_blocks
         self.conv = nn.ModuleList()
         self.conv.append(ConvBlockRes(in_channels, out_channels, momentum))
-        for i in range(n_blocks - 1):
+        for _ in range(n_blocks - 1):
             self.conv.append(ConvBlockRes(out_channels, out_channels, momentum))
         self.kernel_size = kernel_size
         if self.kernel_size is not None:
             self.pool = nn.AvgPool2d(kernel_size=kernel_size)
 
     def forward(self, x):
-        for i, conv in enumerate(self.conv):
+        for conv in self.conv:
             x = conv(x)
         if self.kernel_size is not None:
             return x, self.pool(x)
@@ -279,13 +275,13 @@ class Intermediate(nn.Module):
         self.layers.append(
             ResEncoderBlock(in_channels, out_channels, None, n_blocks, momentum)
         )
-        for i in range(self.n_inters - 1):
+        for _ in range(self.n_inters - 1):
             self.layers.append(
                 ResEncoderBlock(out_channels, out_channels, None, n_blocks, momentum)
             )
 
     def forward(self, x):
-        for i, layer in enumerate(self.layers):
+        for layer in self.layers:
             x = layer(x)
         return x
 
@@ -310,13 +306,13 @@ class ResDecoderBlock(nn.Module):
         )
         self.conv2 = nn.ModuleList()
         self.conv2.append(ConvBlockRes(out_channels * 2, out_channels, momentum))
-        for i in range(n_blocks - 1):
+        for _ in range(n_blocks - 1):
             self.conv2.append(ConvBlockRes(out_channels, out_channels, momentum))
 
     def forward(self, x, concat_tensor):
         x = self.conv1(x)
         x = torch.cat((x, concat_tensor), dim=1)
-        for i, conv2 in enumerate(self.conv2):
+        for conv2 in self.conv2:
             x = conv2(x)
         return x
 
@@ -326,14 +322,14 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.layers = nn.ModuleList()
         self.n_decoders = n_decoders
-        for i in range(self.n_decoders):
+        for _ in range(self.n_decoders):
             out_channels = in_channels // 2
             self.layers.append(
                 ResDecoderBlock(in_channels, out_channels, stride, n_blocks, momentum)
             )
             in_channels = out_channels
 
-    def forward(self, x: torch.Tensor, concat_tensors: List[torch.Tensor]):
+    def forward(self, x: torch.Tensor, concat_tensors: list[torch.Tensor]):
         for i, layer in enumerate(self.layers):
             x = layer(x, concat_tensors[-1 - i])
         return x
