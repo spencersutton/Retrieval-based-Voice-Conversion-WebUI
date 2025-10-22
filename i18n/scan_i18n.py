88707fc7fe222c96904ac7664a209c8dc4714fff
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-10-21 22:47:49 -0400

    Replace glob with Path for file handling in scan_i18n.py

--- i18n/scan_i18n.py
+++ i18n/scan_i18n.py
@@ -1,7 +1,7 @@
 import ast
-import glob
 import json
 from collections import OrderedDict
+from pathlib import Path
 
 
 def extract_i18n_strings(node):
@@ -27,30 +27,21 @@ def extract_i18n_strings(node):
 # for each AST, extract the i18n strings
 
 strings = []
-for filename in glob.iglob("**/*.py", recursive=True):
-    with open(filename, "r") as f:
-        code = f.read()
-        if "I18nAuto" in code:
-            tree = ast.parse(code)
-            i18n_strings = extract_i18n_strings(tree)
-            print(filename, len(i18n_strings))
-            strings.extend(i18n_strings)
+for filename in Path().rglob("*.py"):
+    code = filename.read_text()
+    if "I18nAuto" in code:
+        tree = ast.parse(code)
+        i18n_strings = extract_i18n_strings(tree)
+        print(filename, len(i18n_strings))
+        strings.extend(i18n_strings)
 code_keys = set(strings)
-"""
-n_i18n.py
-gui_v1.py 26
-app.py 16
-infer-web.py 147
-scan_i18n.py 0
-i18n.py 0
-lib/train/process_ckpt.py 1
-"""
+
 print()
 print("Total unique:", len(code_keys))
 
 
-standard_file = "i18n/locale/zh_CN.json"
-with open(standard_file, "r", encoding="utf-8") as f:
+standard_file = Path("i18n/locale/zh_CN.json")
+with standard_file.open("r", encoding="utf-8") as f:
     standard_data = json.load(f, object_pairs_hook=OrderedDict)
 standard_keys = set(standard_data.keys())
 
@@ -69,7 +60,7 @@ code_keys_dict = OrderedDict()
 for s in strings:
     code_keys_dict[s] = s
 
-# write back
-with open(standard_file, "w", encoding="utf-8") as f:
+with standard_file.open("w", encoding="utf-8") as f:
     json.dump(code_keys_dict, f, ensure_ascii=False, indent=4, sort_keys=True)
+    f.write("\n")
     f.write("\n")
