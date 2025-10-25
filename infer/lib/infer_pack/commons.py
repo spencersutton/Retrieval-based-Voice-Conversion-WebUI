79f16a01eb4c357eebe283bf49da8359550a3073
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-10-25 14:26:33 -0400

    refactor: add type hints to functions for improved clarity and type safety

--- infer/lib/infer_pack/commons.py
+++ infer/lib/infer_pack/commons.py
@@ -1,17 +1,26 @@
 import torch
 
 
-def init_weights(m, mean=0.0, std=0.01):
+def init_weights(
+    m: torch.nn.Module,
+    mean: float = 0.0,
+    std: float = 0.01,
+):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
+        assert isinstance(m.weight.data.normal_, torch.nn.Module)
         m.weight.data.normal_(mean, std)
 
 
-def get_padding(kernel_size, dilation=1):
+def get_padding(kernel_size: int, dilation: int = 1):
     return int((kernel_size * dilation - dilation) / 2)
 
 
-def slice_segments(x, ids_str, segment_size=4):
+def slice_segments(
+    x: torch.Tensor,
+    ids_str: torch.Tensor,
+    segment_size: int = 4,
+) -> torch.Tensor:
     ret = torch.zeros_like(x[:, :, :segment_size])
     for i in range(x.size(0)):
         idx_str = ids_str[i]
@@ -20,7 +29,11 @@ def slice_segments(x, ids_str, segment_size=4):
     return ret
 
 
-def slice_segments2(x, ids_str, segment_size=4):
+def slice_segments2(
+    x: torch.Tensor,
+    ids_str: torch.Tensor,
+    segment_size: int = 4,
+) -> torch.Tensor:
     ret = torch.zeros_like(x[:, :segment_size])
     for i in range(x.size(0)):
         idx_str = ids_str[i]
@@ -29,8 +42,12 @@ def slice_segments2(x, ids_str, segment_size=4):
     return ret
 
 
-def rand_slice_segments(x, x_lengths=None, segment_size=4):
-    b, d, t = x.size()
+def rand_slice_segments(
+    x: torch.Tensor,
+    x_lengths: int | None = None,
+    segment_size: int = 4,
+):
+    b, _d, t = x.size()
     if x_lengths is None:
         x_lengths = t
     ids_str_max = x_lengths - segment_size + 1
@@ -39,13 +56,17 @@ def rand_slice_segments(x, x_lengths=None, segment_size=4):
     return ret, ids_str
 
 
-def subsequent_mask(length):
+def subsequent_mask(length: int):
     mask = torch.tril(torch.ones(length, length)).unsqueeze(0).unsqueeze(0)
     return mask
 
 
-@torch.jit.script
-def fused_add_tanh_sigmoid_multiply(input_a, input_b, n_channels):
+@torch.jit.script  # type: ignore
+def fused_add_tanh_sigmoid_multiply(
+    input_a: torch.Tensor,
+    input_b: torch.Tensor,
+    n_channels: torch.Tensor,
+):
     n_channels_int = n_channels[0]
     in_act = input_a + input_b
     t_act = torch.tanh(in_act[:, :n_channels_int, :])
@@ -54,26 +75,12 @@ def fused_add_tanh_sigmoid_multiply(input_a, input_b, n_channels):
     return acts
 
 
-def sequence_mask(length: torch.Tensor, max_length: int | None = None):
+def sequence_mask(
+    length: torch.Tensor,
+    max_length: int | None = None,
+) -> torch.Tensor:
     if max_length is None:
-        max_length = length.max()
+        max_length = int(length.max())
+    assert max_length is not None
     x = torch.arange(max_length, dtype=length.dtype, device=length.device)
     return x.unsqueeze(0) < length.unsqueeze(1)
-
-
-def clip_grad_value_(parameters, clip_value, norm_type=2):
-    if isinstance(parameters, torch.Tensor):
-        parameters = [parameters]
-    parameters = list(filter(lambda p: p.grad is not None, parameters))
-    norm_type = float(norm_type)
-    if clip_value is not None:
-        clip_value = float(clip_value)
-
-    total_norm = 0
-    for p in parameters:
-        param_norm = p.grad.data.norm(norm_type)
-        total_norm += param_norm.item() ** norm_type
-        if clip_value is not None:
-            p.grad.data.clamp_(min=-clip_value, max=clip_value)
-    total_norm = total_norm ** (1.0 / norm_type)
-    return total_norm
