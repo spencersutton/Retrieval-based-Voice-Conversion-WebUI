af17ff3f213e370cad53847cab6733b437bc9185
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-10-25 15:41:57 -0400

    refactor: add type hints to load_language_list and I18nAuto methods for improved clarity

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -3,14 +3,14 @@ import locale
 import os
 
 
-def load_language_list(language):
+def load_language_list(language: str) -> dict[str, str]:
     with open(f"./i18n/locale/{language}.json", encoding="utf-8") as f:
         language_list = json.load(f)
     return language_list
 
 
 class I18nAuto:
-    def __init__(self, language=None) -> None:
+    def __init__(self, language: None = None) -> None:
         if language in ["Auto", None]:
             language = locale.getdefaultlocale()[
                 0
@@ -20,7 +20,7 @@ class I18nAuto:
         self.language = language
         self.language_map = load_language_list(language)
 
-    def __call__(self, key):
+    def __call__(self, key: str) -> str:
         return self.language_map.get(key, key)
 
     def __repr__(self) -> str:
