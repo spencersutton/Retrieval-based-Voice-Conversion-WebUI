f1a0c37056f07a0f1664ec61fd9c18f48bcc7131
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-10-25 12:42:09 -0400

    refactor: enhance type annotations for clarity in merge and extract functions

--- infer/lib/train/process_ckpt.py
+++ infer/lib/train/process_ckpt.py
@@ -1,6 +1,7 @@
 import traceback
 from collections import OrderedDict
 from pathlib import Path
+from typing import Any
 
 import torch
 
@@ -164,11 +165,17 @@ def change_info(path: str, info: str, name: str) -> str:
 
 
 def merge(
-    path1: str, path2: str, alpha1: object, sr: str, f0: object, info: str, name: str
+    path1: str,
+    path2: str,
+    alpha1: torch.Tensor,
+    sr: str,
+    f0: object,
+    info: str,
+    name: str,
 ) -> str:
     try:
 
-        def extract(ckpt: dict[str, dict[str, object]]) -> OrderedDict[str, object]:
+        def extract(ckpt: dict[str, dict[str, Any]]) -> OrderedDict[str, Any]:
             a = ckpt["model"]
             opt: OrderedDict[str, object] = OrderedDict()
             opt["weight"] = {}
@@ -178,8 +185,8 @@ def merge(
                 opt["weight"][key] = a[key]
             return opt
 
-        ckpt1 = torch.load(path1, map_location="cpu")
-        ckpt2 = torch.load(path2, map_location="cpu")
+        ckpt1: dict[str, Any] = torch.load(path1, map_location="cpu")
+        ckpt2: dict[str, Any] = torch.load(path2, map_location="cpu")
         cfg = ckpt1["config"]
         if "model" in ckpt1:
             ckpt1 = extract(ckpt1)
@@ -191,26 +198,21 @@ def merge(
             ckpt2 = ckpt2["weight"]
         if sorted(list(ckpt1.keys())) != sorted(list(ckpt2.keys())):
             return "Fail to merge the models. The model architectures are not the same."
-        opt: OrderedDict[str, object] = OrderedDict()
+        opt: OrderedDict[str, Any] = OrderedDict()
         opt["weight"] = {}
         for key in ckpt1.keys():
             if key == "emb_g.weight" and ckpt1[key].shape != ckpt2[key].shape:
                 min_shape0 = min(ckpt1[key].shape[0], ckpt2[key].shape[0])
-                opt["weight"][key] = (
-                    alpha1 * (ckpt1[key][:min_shape0].float())
-                    + (1 - alpha1) * (ckpt2[key][:min_shape0].float())
-                ).half()
+                merged_emb_weight = alpha1 * (ckpt1[key][:min_shape0].float()) + (
+                    1 - alpha1
+                ) * (ckpt2[key][:min_shape0].float())
+                opt["weight"][key] = (merged_emb_weight).half()
             else:
                 opt["weight"][key] = (
                     alpha1 * (ckpt1[key].float()) + (1 - alpha1) * (ckpt2[key].float())
                 ).half()
 
         opt["config"] = cfg
-        """
-        if(sr=="40k"):opt["config"] = [1025, 32, 192, 192, 768, 2, 6, 3, 0, "1", [3, 7, 11], [[1, 3, 5], [1, 3, 5], [1, 3, 5]], [10, 10, 2, 2], 512, [16, 16, 4, 4,4], 109, 256, 40000]
-        elif(sr=="48k"):opt["config"] = [1025, 32, 192, 192, 768, 2, 6, 3, 0, "1", [3, 7, 11], [[1, 3, 5], [1, 3, 5], [1, 3, 5]], [10,6,2,2,2], 512, [16, 16, 4, 4], 109, 256, 48000]
-        elif(sr=="32k"):opt["config"] = [513, 32, 192, 192, 768, 2, 6, 3, 0, "1", [3, 7, 11], [[1, 3, 5], [1, 3, 5], [1, 3, 5]], [10, 4, 2, 2, 2], 512, [16, 16, 4, 4,4], 109, 256, 32000]
-        """
         opt["sr"] = sr
         opt["f0"] = 1 if f0 == i18n("是") else 0
         opt["info"] = info
